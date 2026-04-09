a42ef9bf8995bce93819782dd1be50efd5ec3cd0
Luke Mitchell <70588025+lukemitch23@users.noreply.github.com>
2026-04-09 22:19:04 +0100

    Updated light control header and C files - minus lat implementation

--- LIGHT_CONTROL.c
+++ LIGHT_CONTROL.c
@@ -1,11 +1,83 @@
 /**********************************************************************************************************************************
-*
+ *
  *  File name: LIGHT_CONTROL.c
  *  Date: 18/03/2026
  *
- *  Description:
+ *  Description: C file for the light control function. Function implementation allowing the user to control the headlights as
+ *				 well as toggle the reverse lights and the indicators when turning. Public functions allow control of the light
+ *				 groups with individual lights controlled by local functions.
  *
 **********************************************************************************************************************************/
 
+/* Include header file */
 #include "LIGHT_CONTROL.h"
 
+void LIGHT_CONTROL__Init(void){
+	/* Initalise all the lights to off */
+	Front_Left_White(OFF);
+	Front_Right_White(OFF);
+	Back_Left_White(OFF);
+	Back_Right_White(OFF);
+	Back_Left_Red(OFF);
+	Back_Right_Red(OFF);
+	Front_Left_Yellow(OFF);
+	Front_Right_Yellow(OFF);
+	Back_Left_Yellow(OFF);
+	Back_Right_Yellow(OFF);
+}
+
+void LIGHT_CONTROL__Head_Lights(bool set){
+	Front_Left_White(set);
+	Front_Right_White(set);
+	Back_Left_White(set);
+	Back_Right_White(set);
+}
+
+
+
+void LIGHT_CONTROL__Reverse_Lights(bool set) {
+	/* Assume correct input and set the red lights to the choice */
+	Back_Left_Red(set);
+	Back_Right_Red(set);
+}
+
+void LIGHT_CONTROL__Turn_Left(bool set) {
+	/* Ensure right indicators are off */
+	Front_Right_Yellow(OFF);
+	Back_Right_Yellow(OFF);
+	Front_Left_Yellow(set);
+	Front_Right_Yellow(set);
+}
+
+void LIGHT_CONTROL__Turn_Right(bool set) {
+	/* Ensure left inidcators are off */
+	Front_Left_Yellow(OFF);
+	Back_Left_Yellow(OFF);
+	Front_Right_Yellow(set);
+	Front_Right_Yellow(set);
+}
+
+void LIGHT_CONTROL__All_Off(void){
+	/* Set all lights to off */
+	Front_Left_White(OFF);
+	Front_Right_White(OFF);
+	Back_Left_White(OFF);
+	Back_Right_White(OFF);
+	Back_Left_Red(OFF);
+	Back_Right_Red(OFF);
+	Front_Left_Yellow(OFF);
+	Front_Right_Yellow(OFF);
+	Back_Left_Yellow(OFF);
+	Back_Right_Yellow(OFF);
+}
+
+void Front_Left_White(bool set);
+void Front_Right_White(bool set);
+void Back_Left_White(bool set);
+void Back_Right_White(bool set);
+void Back_Left_Red(bool set);
+void Back_Right_Red(bool set);
+void Front_Left_Yellow(bool set);
+void Front_Right_Yellow(bool set);
+void Back_Left_Yellow(bool set);
+void Back_Right_Yellow(bool set);
--- LIGHT_CONTROL.h
+++ LIGHT_CONTROL.h
@@ -1,17 +1,226 @@
 /**********************************************************************************************************************************
-*
+ *
  *  File name: LIGHT_CONTROL.h
  *  Date: 18/03/2026
  *
- *  Description:
+ *  Description: Header file for the light control function. Function headers that will allow the user to control the
+ *				 robots lights. Also defining the ON and OFF constants.
  *
 **********************************************************************************************************************************/
 
-void LIGHT_CONTROL__Init(void){}
+/* Define constants for on and off */
+#define ON 1
+#define OFF 0
 
-void LIGHT_CONTROL__Update_Status(void){}
+/**********************************************************************************************************************************
+*
+*	Light Connections:
+*		Lights on port A (0-7) (Pins 2-7,13-14) & port E (0-2)(Pins 8-10)
+*		Port A:
+*			0 -
+*			1 -
+*			2 -
+*			3 -
+*			4 -
+*			5 -
+*			6 -
+*			7 -
+*
+*		Port E:
+*			0 -
+* 			1 -
+*			2 - Spare
+*
+**********************************************************************************************************************************/
 
-void LIGHT_CONTROL__Toggle_Running_Light(void){}
+/**********************************************************************************************************************************
+ *
+ *  Function name: LIGHT_CONTROL__Init
+ *
+ *  Input: None
+ *  Outputs: None
+ *
+ *  Usage: Initialised the lights all to off
+ *
+**********************************************************************************************************************************/
+void LIGHT_CONTROL__Init(void);
 
-void Get_Status(void){}
-void Set_Led_Colour(void){}
+/**********************************************************************************************************************************
+ *
+ *  Function name: LIGHT_CONTROL__Head_Lights
+ *
+ *  Input: bool set
+ *  Outputs: None
+ *
+ *  Usage: Turn the headlights on or off depending on user input
+ *
+**********************************************************************************************************************************/
+void LIGHT_CONTROL__Head_Lights(bool set);
+
+/**********************************************************************************************************************************
+ *
+ *  Function name: LIGHT_CONTROL__Reverse_Lights
+ *
+ *  Input: bool set
+ *  Outputs: None
+ *
+ *  Usage: Turn the reversing lights on or off depending on user input
+ *
+**********************************************************************************************************************************/
+void LIGHT_CONTROL__Reverse_Lights(bool set);
+
+/**********************************************************************************************************************************
+ *
+ *  Function name: LIGHT_CONTROL__Turn_Left
+ *
+ *  Input: bool set
+ *  Outputs: None
+ *
+ *  Usage: Turn the left indicator on or off when turning for calling from an interrupt
+ *
+**********************************************************************************************************************************/
+void LIGHT_CONTROL__Turn_Left(bool set);
+
+/**********************************************************************************************************************************
+ *
+ *  Function name: LIGHT_CONTROL__Turn_Right
+ *
+ *  Input: bool set
+ *  Outputs: None
+ *
+ *  Usage: Turn the right indicator on or off when turning for calling from an interrupt
+ *
+**********************************************************************************************************************************/
+void LIGHT_CONTROL__Turn_Right(bool set);
+
+/**********************************************************************************************************************************
+ *
+ *  Function name: LIGHT_CONTROL__All_Off
+ *
+ *  Input: None
+ *  Outputs: None
+ *
+ *  Usage: Set all lights off
+ *
+**********************************************************************************************************************************/
+void LIGHT_CONTROL__All_Off(void);
+
+/**********************************************************************************************************************************
+ *
+ *  Function name: Front_Left_White
+ *
+ *  Input: None
+ *  Outputs: None
+ *
+ *  Usage: Set the light on or off based on the user input
+ *
+**********************************************************************************************************************************/
+void Front_Left_White(bool set);
+
+/**********************************************************************************************************************************
+ *
+ *  Function name: Front_Right_White
+ *
+ *  Input: None
+ *  Outputs: None
+ *
+ *  Usage: Set the light on or off based on the user input
+ *
+**********************************************************************************************************************************/
+void Front_Right_White(bool set);
+
+/**********************************************************************************************************************************
+ *
+ *  Function name: Back_Left_White
+ *
+ *  Input: None
+ *  Outputs: None
+ *
+ *  Usage: Set the light on or off based on the user input
+ *
+**********************************************************************************************************************************/
+void Back_Left_White(bool set);
+
+/**********************************************************************************************************************************
+ *
+ *  Function name: Back_Right_White
+ *
+ *  Input: None
+ *  Outputs: None
+ *
+ *  Usage: Set the light on or off based on the user input
+ *
+**********************************************************************************************************************************/
+void Back_Right_White(bool set);
+
+/**********************************************************************************************************************************
+ *
+ *  Function name: Back_Left_Red
+ *
+ *  Input: None
+ *  Outputs: None
+ *
+ *  Usage: Set the light on or off based on the user input
+ *
+**********************************************************************************************************************************/
+void Back_Left_Red(bool set);
+
+/**********************************************************************************************************************************
+ *
+ *  Function name: Back_Right_Red
+ *
+ *  Input: None
+ *  Outputs: None
+ *
+ *  Usage: Set the light on or off based on the user input
+ *
+**********************************************************************************************************************************/
+void Back_Right_Red(bool set);
+
+/**********************************************************************************************************************************
+ *
+ *  Function name: Front_Left_Yellow
+ *
+ *  Input: None
+ *  Outputs: None
+ *
+ *  Usage: Set the light on or off based on the user input
+ *
+**********************************************************************************************************************************/
+void Front_Left_Yellow(bool set);
+
+/**********************************************************************************************************************************
+ *
+ *  Function name: Front_Right_Yellow
+ *
+ *  Input: None
+ *  Outputs: None
+ *
+ *  Usage: Set the light on or off based on the user input
+ *
+**********************************************************************************************************************************/
+void Front_Right_Yellow(bool set);
+
+/**********************************************************************************************************************************
+ *
+ *  Function name: Back_Left_Yellow
+ *
+ *  Input: None
+ *  Outputs: None
+ *
+ *  Usage: Set the light on or off based on the user input
+ *
+**********************************************************************************************************************************/
+void Back_Left_Yellow(bool set);
+
+/**********************************************************************************************************************************
+ *
+ *  Function name: Back_Right_Yellow
+ *
+ *  Input: None
+ *  Outputs: None
+ *
+ *  Usage: Set the light on or off based on the user input
+ *
+**********************************************************************************************************************************/
+void Back_Right_Yellow(bool set);
